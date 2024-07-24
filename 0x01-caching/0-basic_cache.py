70461c3d58bd2cfbaa8edb02a7f6dd5fa93a3df2
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-07-24 21:16:09 +0000

    basic cache using inheritance

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+       Basic cache class that inherits from BaseCaching class
+    """
+
+    def put(self, key, item):
+        """ method that assigns the key and item
+            to the dict self.cache_data
+            Do nothing if key or item is None
+        """
+        if None in (key, item):
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        else:
+            try:
+                self.cache_data[key]
+                return self.cache_data[key]
+            except KeyError as e:
+                return None
