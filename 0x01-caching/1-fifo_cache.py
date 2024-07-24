5a35782756662bd5fd8c0397ede8dae103348844
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-07-24 21:56:56 +0000

    FIFO poping from cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" FIFO Caching Module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+        a class FIFOCache that inherits from BaseCaching
+        and is a caching system
+    """
+
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+        self.list_dict = list(self.cache_data.keys())
+
+    def put(self, key, item):
+        """
+            assign to the dictionary self.cache_data
+            the item value for the key
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.list_dict = list(self.cache_data.keys())
+            print(f"DISCARD {self.list_dict[0]}")
+            del self.cache_data[self.list_dict[0]]
+
+    def get(self, key):
+        """
+            return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key, None)
